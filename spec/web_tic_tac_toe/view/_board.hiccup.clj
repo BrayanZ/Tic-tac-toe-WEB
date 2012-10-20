492a0807f1d41d07acc1961f23d81801d4ab8259
BrayanZ <brayan@pernix-solutions.com>
2012-10-19 18:01:44 -0600

    delete unnecessary files

--- spec/web_tic_tac_toe/view/_board.hiccup.clj
+++
@@ -1,6 +0,0 @@
-[:table {:class "table table-bordered table-condensed span3"}
- (let [board (:board *view-context*)]
-   (for [row board]
-     [:tr
-      (for [cell row]
-        [:td (get_cell cell) ])]))]
